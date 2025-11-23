f8236e5ea520f7263e5914ecfdd35e4ac140306f
wu0402 <wuyoyotaiwan@gmail.com>
2025-11-23 22:55:37 +0800

    Add files via upload

--- HW4_6-11/6-11.c
+++ HW4_6-11/6-11.c
@@ -13,7 +13,6 @@ int main()
     printf("Original array:\n");
     printArray(a, SIZE);
 
-    // ㊣э▆逼猭
     improvedBubbleSort(a, SIZE);
 
     printf("\nSorted array:\n");
@@ -23,24 +22,17 @@ int main()
     return 0;
 }
 
-// э▆獁逼猭
 void improvedBubbleSort(int array[], int size)
 {
-    int pass; // 北近计
-    int j;    // 北ゑ耕
-    int hold; // ノユ传既跑计
-    int swapped; // ノ癘魁赣近琌Τ祇ネユ传 (肈ヘ b 璶―)
+    int pass;
+    int j;
+    int hold;
+    int swapped;
 
-    // 糷癹伴北苯磞近计
     for (pass = 0; pass < size - 1; pass++)
     {
-        // 肈ヘ b 璶―–近秨﹍玡盢ユ传篨夹砞 0 (安)
         swapped = 0; 
 
-        // ず糷癹伴ゑ耕籔ユ传
-        // 肈ヘ a 璶―э癹伴兵ン
-        // ﹍琌 j < size - 1瞷эΘ j < size - 1 - pass
-        // –筁近 (pass 糤)程计竒↖┏场ぃ惠璶ゑ耕
         for (j = 0; j < size - 1 - pass; j++)
         {
             if (array[j] > array[j + 1])
